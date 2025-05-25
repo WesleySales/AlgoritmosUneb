bdfeb0ff0d229fa5c8edce810da9d438cb792d5a
Wesley Sales <126846974+WesleySales@users.noreply.github.com>
2025-05-25 18:57:42 -0300

    Update exercicio10.cpp

--- Lista_Exercicios_UNEB/exercicio10.cpp
+++ Lista_Exercicios_UNEB/exercicio10.cpp
@@ -5,25 +5,22 @@ int main(){
 	int numAnterior, numAtual;
 	int seqAtual=0, seqMaior;
 	
+	printf("Digite '-1' para encerrar a qualquer momento!\n\n");
+	
 	while(numAtual>=0){
-		printf("Digite um numero: \n\n");
+		printf("Digite um numero: ");
 		scanf("%d", &numAtual);
 		
 		if(numAtual>numAnterior){
 			seqAtual++;
-			printf("O numero atual (%d) eh MAIOR que o antecessor (%d): sequencia atualizada: %d",numAtual, numAnterior,seqAtual);
 		}
 		else if(numAtual>0){
 			seqMaior = seqAtual;
 			seqAtual=1;
-			printf("O numero atual (%d) eh MENOR que o antecessor (%d): sequencia atualizada: %d",numAtual, numAnterior,seqAtual);
 		}		
 		
 		numAnterior = numAtual;
-		
-		printf("\n\nDigite '-1' para encerrar!\n\n");
 	}
 	if(seqAtual>seqMaior) seqMaior=seqAtual;
-//	seq = seq>seqMaior? seq:seqMaior;
 	printf("A maior sequencia crescente foi %d",seqMaior);
 }
