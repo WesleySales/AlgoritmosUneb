043350170504ed0758487fed4e9967de6cfad903
Wesley Sales <126846974+WesleySales@users.noreply.github.com>
2025-05-28 10:51:04 -0300

    Atualizar o exercicio8.cpp

--- Lista_Exercicios_UNEB/exercicio8.cpp
+++ Lista_Exercicios_UNEB/exercicio8.cpp
@@ -1,23 +1,31 @@
 #include <stdio.h>
 
-int main() {
-    
-    float somaA =0,somaB=0;
-    
-    for(int i=0;i<100;i++){
-    	if(i%2!=0){
-    		somaA += 1.0/i;
-		}
-	}
-	
-	printf("a) %.2f\n", somaA);
-	
-	for(int i=0;i<20;i++){
-    	if(i%2!=0){
-    		somaB += 1.0/i;
-    		somaB -= 1.0/(i+2);
-    		i+=2;
-		}
-	}
-	printf("b) %.2f", somaB);
+int main()
+{
+float somaA=0, somaB=0;
+int vetor[100], cont=0;
+
+for(int i=0; i<100;i++){
+     if(i%2!=0){
+          somaA += 1.0/i;
+          vetor[cont] = i;
+          cont++;
+     }
+}
+
+printf("a) %.4f\n", somaA);
+
+//Letra b
+
+for(int i=0; i<20;i++){
+     if(i%2==0){
+          somaB+= 1.0/vetor[i];
+          //printf("+ 1/%d", vetor[ i ]);
+     } else {
+          somaB-= 1.0/vetor[i];
+          //printf("- 1/%d", vetor[ i ]);
+     }
+}
+
+printf("b) %.4f", somaB);
 }
